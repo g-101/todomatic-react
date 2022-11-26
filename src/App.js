49cd6d20a922e23c8d100b757c7f5b80c9b3fca7
genesis-g101 <gyreplicant@gmail.com>
2022-11-26 10:48:37 -0300

    separando os componentes Form e FilterButton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+import React from 'react';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
 
-function App() {
+function App(props) {
+  // const taskList = props.tasks?.map(task => task.name);
+  // const taskList = props.tasks?.map(task => (
+  //   <Todo id={task.id} name={task.name} completed={task.completed} />
+  // ));
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form />
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
@@ -39,12 +30,10 @@ function App() {
         </button>
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        <Todo />
+      <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
+        {props.tasks.map(task => (
+          <Todo key={task.id} id={task.id} name={task.name} completed={task.completed} />
+        ))}
       </ul>
     </div>
   );
