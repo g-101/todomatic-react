5b881238dd13ea57c41f9835a5825401fe18b113
genesis-g101 <gyreplicant@gmail.com>
2022-11-26 11:22:20 -0300

    colocando o FilterButton no App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
 
 function App(props) {
-  // const taskList = props.tasks?.map(task => task.name);
   // const taskList = props.tasks?.map(task => (
   //   <Todo id={task.id} name={task.name} completed={task.completed} />
   // ));
@@ -13,23 +12,11 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{props.tasks.length} tasks remaining</h2>
       <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
         {props.tasks.map(task => (
           <Todo key={task.id} id={task.id} name={task.name} completed={task.completed} />
